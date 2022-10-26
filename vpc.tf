3b32a4f27b92a7c4de91be607362e2c629e17c9c
Berret Terry <berretterry@gmail.com>
2022-10-26 12:44:43 -0700

    final adjustments and plan worked

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,9 @@
+resource "aws_eip" "nat" {
+    count = 3
+
+    vpc = true
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.14.2"
@@ -7,22 +13,16 @@ module "vpc" {
   
   azs = ["us-west-2a", "us-west-2b", "us-west-2c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.4.0/24"]
+  public_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
 
-  single_nat_gateway = true
-  reuse_nat_ips = true
-  external_nat_ip_ids = ["${aws_eip.nat.*.id}"]
+  single_nat_gateway = false
+  external_nat_ip_ids = "${aws_eip.nat.*.id}"
 
   tags = {
     Terraform = "true"
   }
 }
 
-resource "aws_eip" "nat" {
-    count = 1
-
-    vpc = true
-}
